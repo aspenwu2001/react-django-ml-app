968ece5d52631f7137f34bfe1fe120036ae061dc
981526092 <55764260+981526092@users.noreply.github.com>
2021-03-16 17:23:45 +0000

    Add files via upload

--- AvanadeIHP/Algorithm/algorithm.py
+++ AvanadeIHP/Algorithm/algorithm.py
@@ -9,9 +9,9 @@ from sklearn.decomposition import PCA
 
 class biasAlgorithm:
 
-    def __init__(self,model,biasPairAddress):
+    def __init__(self,model,type):
         self.__model = model
-        self.__biased_word_pairs =self.__readBiasPair(biasPairAddress)
+        self.__biased_word_pairs = self.__defaultBiasPair(type)
         self.__process_data()
 
     def estimate(self,index):
@@ -26,12 +26,24 @@ class biasAlgorithm:
         else:
             return "Unbiased"
 
+    def __defaultBiasPair(self,index):
+        if index == 1:#Gender
+            return [('cowgirl', 'cowboy'), ('cowgirls', 'cowboys'), ('camerawoman', 'cameraman'), ('busgirl', 'busboy'), ('busgirls', 'busboys'), ('bellgirl', 'bellboy'), ('bellgirls', 'bellboys'), ('barwoman', 'barman'), ('barwomen', 'barmen'), ('seamstress', 'tailor'), ('seamstress', 'tailors'), ('princess', 'prince'), ('princesses', 'princes'), ('governess', 'governor'), ('governesses', 'governors'), ('adultress', 'adultor'), ('adultresses', 'adultors'), ('godess', 'god'), ('godesses', 'gods'), ('hostess', 'host'), ('hostesses', 'hosts'), ('abbess', 'abbot'), ('abbesses', 'abbots'), ('actress', 'actor'), ('actresses', 'actors'), ('spinster', 'bachelor'), ('spinsters', 'bachelors'), ('baroness', 'baron'), ('barnoesses', 'barons'), ('belle', 'beau'), ('belles', 'beaus'), ('bride', 'bridegroom'), ('brides', 'bridegrooms'), ('sister', 'brother'), ('sisters', 'brothers'), ('duchess', 'duke'), ('duchesses', 'dukes'), ('empress', 'emperor'), ('empresses', 'emperors'), ('enchantress', 'enchanter'), ('mother', 'father'), ('mothers', 'fathers'), ('fiancee', 'fiance'), ('fiancees', 'fiances'), ('nun', 'priest'), ('nuns', 'priests'), ('lady', 'gentleman'), ('ladies', 'gentlemen'), ('grandmother', 'grandfather'), ('grandmothers', 'grandfathers'), ('headmistress', 'headmaster'), ('headmistresses', 'headmasters'), ('heroine', 'hero'), ('heroines', 'heros'), ('lass', 'lad'), ('lasses', 'lads'), ('landlady', 'landlord'), ('landladies', 'landlords'), ('female', 'male'), ('females', 'males'), ('woman', 'man'), ('women', 'men'), ('maidservant', 'manservant'), ('maidservants', 'manservants'), ('marchioness', 'marquis'), ('masseuse', 'masseur'), ('masseuses', 'masseurs'), ('mistress', 'master'), ('mistresses', 'masters'), ('nun', 'monk'), ('nuns', 'monks'), ('niece', 'nephew'), ('nieces', 'nephews'), ('priestess', 'priest'), ('priestesses', 'priests'), ('sorceress', 'sorcerer'), ('sorceresses', 'sorcerers'), ('stepmother', 'stepfather'), ('stepmothers', 'stepfathers'), ('stepdaughter', 'stepson'), ('stepdaughters', 'stepsons'), ('stewardess', 'steward'), ('stewardesses', 'stewards'), ('aunt', 'uncle'), ('aunts', 'uncles'), ('waitress', 'waiter'), ('waitresses', 'waiters'), ('widow', 'widower'), ('widows', 'widowers'), ('witch', 'wizard'), ('witches', 'wizards'), ('airman', 'airwoman'), ('airmen', 'airwomen'), ('airwoman', 'airman'), ('airwomen', 'airmen'), ('girl', 'boy'), ('girls', 'boys'), ('brother', 'sister'), ('brothers', 'sisters'), ('businesswoman', 'businessman'), ('businesswomen', 'businessmen'), ('chairwoman', 'chairman'), ('chairwomen', 'chairman'), ('chick', 'dude'), ('chicks', 'dudes'), ('mom', 'dad'), ('moms', 'dads'), ('mommy', 'daddy'), ('mommies', 'daddies'), ('daughter', 'son'), ('daughters', 'sons'), ('gal', 'guy'), ('gals', 'guys'), ('lady', 'mentleman'), ('granddaughter', 'grandson'), ('granddaughters', 'grandsons'), ('groom', 'bride'), ('grooms', 'brides'), ('girl', 'guy'), ('girls', 'guys'), ('she', 'he'), ('himself', 'herself'), ('her', 'him'), ('herself', 'himself'), ('her', 'his'), ('wife', 'husband'), ('wives', 'husbands'), ('queen', 'king'), ('queens', 'kings'), ('lady', 'lord'), ('ladies', 'lords'), ("ma'am", 'sir'), ('miss', 'sir'), ('mrs.', 'mr.'), ('ms.', 'mr.'), ('policewoman', 'policeman'), ('prince', 'princess'), ('princes', 'princesses'), ('spokeswoman', 'spokesman'), ('spokeswomen', 'spokesmen'), ('womanly', 'manly'), ('girlish', 'boyish'), ('feminine', 'masculine'), ('Mary', 'John')]
+        elif index == 2:#Age
+            return [('ruth', 'taylor'), ('william', 'jamie'), ('horace', 'daniel'), ('mary', 'aubrey'), ('susie', 'alison'), ('amy', 'miranda'), ('john', 'jacob'), ('henry', 'arthur'), ('edward', 'aaron'), ('elizabeth', 'ethan'), ('wrinkled', 'unwrinkled'), ('wrinkled', 'placid'), ('unattractive', 'charming'), ('untidy', 'neat'), ('unattractive', 'attractive'), ('epicophosis', 'hearing'), ('deaf', 'hearing'), ('earless', 'hearing'), ('sedentary', 'energetic'), ('inactive', 'active'), ('sick', 'healthy'), ('fragile', 'strong'), ('tired', 'energized'), ('unhealthy', 'healthy'), ('inflexibility', 'flexibility'), ('constraint', 'flexibility'), ('old-fashioned', 'modern'), ('ancient', 'current'), ('olden', 'young'), ('old', 'young'), ('outdated', 'new'), ('outdated', 'fresh'), ('cautiousness', 'carelessness'), ('cautiousness', 'indiscretion'), ('self-discipline', 'blandness'), ('dejected', 'cheerful')]
+        elif index == 3:#Racial
+            return [('brad', 'darnell'), ('brendan', 'hakim'), ('geoffrey', 'jermaine'), ('greg', 'kareem'), ('brett', 'jamal'), ('matthew', 'leroy'), ('neil', 'tyrone'), ('todd', 'rasheed'), ('nancy', 'yvette'), ('amanda', 'malika'), ('emily', 'latonya'), ('rachel', 'jasmine'), ('alejandro', 'jermaine'), ('pancho', 'kareem'), ('bernardo', 'latonya'), ('pedro', 'jasmine'), ('octavio', 'hakim'), ('rodrigo', 'darnell'), ('ricardo', 'jamal'), ('augusto', 'leroy'), ('carmen', 'tyrone'), ('katia', 'rasheed'), ('marcella', 'yvette'), ('sofia', 'malika')]
+
+
     def __readBiasPair(self,address):
         pairlist = list(csv.reader(open(address,'r')))
         temp = []
         for x in pairlist:
            temp.append((x[0],x[1]))
         return temp
+
+    def getBiasPair(self):
+        return self.__biased_word_pairs
 
     def changeBiasPair(self,address):
         self.__biased_word_pairs =self.__readBiasPair(address)
@@ -48,7 +60,7 @@ class biasAlgorithm:
             try:
                 biasedPairs.append((self.__model[pair[0]], self.__model[pair[1]]))
             except:
-                print(pair[0] + " and " + pair[1] + "are not in the web embedding")
+                print(pair[0] + " and " + pair[1] + "are not in the word embedding")
 
         biases = []
         reversed_biases = []
@@ -122,3 +134,7 @@ class biasAlgorithm:
         if word in synonyms:
             synonyms.remove(word)
         return list(synonyms)
+
+if __name__ == "__main__":
+    vv = biasAlgorithm(None)
+    print(vv.getBiasPair())
